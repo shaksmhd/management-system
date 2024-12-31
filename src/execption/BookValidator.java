6ab565ddebd77c8bae9903bf1aaf31f8009d2565
shaksmhd <m_shaka@yahoo.com.com>
2024-12-31 13:22:31 +0100

    changes on book validator

--- src/execption/BookValidator.java
+++ src/execption/BookValidator.java
@@ -2,11 +2,21 @@ package execption;
 
 import model.Book;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BookValidator {
+    private static final Set<String> isbnSet = new HashSet<>();
+
     public static void validate(Book book) throws InvalidIsbnException {
         // Validate ISBN format (example: checking length)
         if (book.getIsbn().length() != 5) {
             throw new InvalidIsbnException("Invalid ISBN format.");
+        }
+
+        // Check for unique ISBN
+        if (!isbnSet.add(book.getIsbn())) {
+            throw new InvalidIsbnException("ISBN must be unique.");
         }
     }
 }
